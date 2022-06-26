f45593efff763b993359d0ca0528d7d622103b6a
shs <hs.seo@kisos.net>
2022-06-26 15:11:50 +0900

    FIX : props명칭 변경

--- src/pages/ShoppingBasket/BasketRowz/BasketRow.js
+++ src/pages/ShoppingBasket/BasketRowz/BasketRow.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './BasketRow.scss';
-const BasketRow = ({ name, size, quantities, price }) => {
+const BasketRow = ({ name, size, quantity, price }) => {
   return (
     <>
       <div className="info">{name}</div>
       <div className="info size">{size}</div>
       <select className="info quantities">
         {[1, 2, 3, 4, 5].map(el => (
-          <option key={el} value={el} selected={el === quantities}>
+          <option key={el} value={el} selected={el === quantity}>
             {el}
           </option>
         ))}
