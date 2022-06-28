46420103ea89126fafe2e2c2acb23d80e7e5d581
shs <hs.seo@kisos.net>
2022-06-28 22:52:06 +0900

    FIX : Layout 컴포넌트에서 장바구니 아이콘 제거

--- src/pages/Layout/Layout.js
+++ src/pages/Layout/Layout.js
@@ -12,7 +12,6 @@ const Layout = ({ children }) => {
           setShoppingBasketShow={setShoppingBasketShow}
         />
       )}
-      <ShoppingCart setShoppingBasketShow={setShoppingBasketShow} />
       {children}
     </>
   );
