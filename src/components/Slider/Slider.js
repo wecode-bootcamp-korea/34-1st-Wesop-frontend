630cda083e3fb650b4f246a4749502ffbff39a61
조예지 <jennycho@joyejiui-MacBookPro.local>
2022-06-29 13:16:21 +0900

    mock data 수정 및 적용

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -3,11 +3,11 @@ import './Slider.scss';
 
 const IMG_WIDTH = 389;
 
-const Slider = () => {
+const Slider = ({ recommend }) => {
   const [move, setMove] = useState(0);
 
   const moveRight = () => {
-    if (move >= -(ITEM_CARD_DATA.length * IMG_WIDTH) + IMG_WIDTH * 3) {
+    if (move >= -(recommend.length * IMG_WIDTH) + IMG_WIDTH * 3) {
       setMove(move => move - IMG_WIDTH);
     }
   };
@@ -40,13 +40,13 @@ const Slider = () => {
           <div className="recommendTitleBox">
             <p>함께 사용하기 좋은 제품</p>
           </div>
-          {ITEM_CARD_DATA.map(el => {
+          {recommend.map(item => {
             return (
               <ItemCard
-                key={el.id}
-                name={el.name}
-                img={el.img}
-                description={el.description}
+                key={item.product_id}
+                name={item.name}
+                img={item.product_image_url}
+                description={item.product_content}
               />
             );
           })}
@@ -67,44 +67,5 @@ const ItemCard = ({ img, name, description }) => {
     </div>
   );
 };
-
-const ITEM_CARD_DATA = [
-  {
-    id: 1,
-    name: '파슬리 씨드 안티 옥시던트 인텐스 세럼',
-    description: '지속적인 수분을 공급하는 항산화 세럼',
-    img: '/images/Aesop_Skin_Parsley_Seed_Anti-Oxidant_Intense_Serum_60mL_Web_Large_684x668px.webp',
-  },
-  {
-    id: 2,
-    name: '비 앤 티 밸런싱 토너',
-    description: '균형감, 수분 공급, 가벼운 마무리',
-    img: '/images/AesopSkin-B-Tea-Balancing-Toner-200mL-large.webp',
-  },
-  {
-    id: 3,
-    name: '파슬리 씨드 안티 옥시던트 스킨케어 키트',
-    description: '항산화 성분이 풍부한 앙상블',
-    img: '/images/Aesop_Kits_Parsley_Seed_Anti-Oxidant_Skin_Care_Kit_Web_Large_1584x962px.webp',
-  },
-  {
-    id: 1,
-    name: '파슬리 씨드 안티 옥시던트 인텐스 세럼',
-    description: '지속적인 수분을 공급하는 항산화 세럼',
-    img: '/images/Aesop_Skin_Parsley_Seed_Anti-Oxidant_Intense_Serum_60mL_Web_Large_684x668px.webp',
-  },
-  {
-    id: 2,
-    name: '비 앤 티 밸런싱 토너',
-    description: '균형감, 수분 공급, 가벼운 마무리',
-    img: '/images/AesopSkin-B-Tea-Balancing-Toner-200mL-large.webp',
-  },
-  {
-    id: 3,
-    name: '파슬리 씨드 안티 옥시던트 스킨케어 키트',
-    description: '항산화 성분이 풍부한 앙상블',
-    img: '/images/Aesop_Kits_Parsley_Seed_Anti-Oxidant_Skin_Care_Kit_Web_Large_1584x962px.webp',
-  },
-];
 
 export default Slider;
--- src/pages/DetailItem/DetailItem.js
+++ src/pages/DetailItem/DetailItem.js
@@ -10,13 +10,13 @@ const DetailItem = () => {
     fetch('/data/mockDataDetailItem.json')
       .then(response => response.json())
       .then(result => {
-        console.log(result);
         setDetailItems(result);
         setSelectedItem({
           id: result.product.product_option[0].id,
           image_url: result.product.product_option[0].image_url,
-          price: result.product.product_option[0].price,
-          size_mL: result.product.product_option[0].size_mL,
+          price: '₩ ' + Math.floor(result.product.product_option[0].price),
+          sizes_mL: result.product.product_option[0].sizes_mL,
+          is_include_pump: result.product.product_option[0].is_include_pump,
         });
       });
   }, []);
@@ -36,9 +36,9 @@ const DetailItem = () => {
         <div className="descriptionBox">
           <div className="itemContainer">
             <div>
-              <span>스킨</span>
-              <span> · </span>
-              <span>클렌저</span>
+              <span>{detailItems.product.category}</span>
+              {/* <span> · </span>
+              <span>클렌저</span> */}
             </div>
             <div className="titleBox">
               <div className="title">{detailItems.product.name}</div>
@@ -47,7 +47,7 @@ const DetailItem = () => {
             <div className="categories">
               {detailItems.product.features[0].feature.map(description => {
                 return (
-                  <div className="category">
+                  <div className="category" key={description.id}>
                     <p>{description.feature}</p>
                     <p className="element">{description.content}</p>
                   </div>
@@ -57,17 +57,26 @@ const DetailItem = () => {
             <div className="sizeBox">
               <p>사이즈</p>
               {detailItems.product.product_option.map(
-                ({ id, size_mL, image_url, price }) => {
+                ({ id, sizes_mL, image_url, price, is_include_pump }) => {
                   return (
                     <label key={id}>
-                      <input
-                        type="radio"
-                        checked={selectedItem.id === id}
-                        onClick={() =>
-                          setSelectedItem({ id, image_url, price })
-                        }
-                      />
-                      {size_mL}
+                      {detailItems.product.product_option.length === 1 ? (
+                        <></>
+                      ) : (
+                        <input
+                          type="radio"
+                          checked={selectedItem.id === id}
+                          onClick={() =>
+                            setSelectedItem({
+                              id,
+                              image_url,
+                              price,
+                              is_include_pump,
+                            })
+                          }
+                        />
+                      )}
+                      <div className="sizes">{sizes_mL}mL</div>
                     </label>
                   );
                 }
@@ -98,18 +107,20 @@ const DetailItem = () => {
       </div>
       <div className="howToUseContainer">
         <div className="itemImage">
-          <p>{detailItems.product.additional_image_url}</p>
+          <img src={detailItems.product.additional_image_url} />
         </div>
         <div className="howToUseBox">
           <div className="howToUseWrapper">
             <div className="howToUseTitle">
               {detailItems.product.additional_name}
             </div>
-            <div className="howToUseText">{detailItems.additional_content}</div>
+            <div className="howToUseText">
+              {detailItems.product.additional_content}
+            </div>
             <div className="categories">
               {detailItems.product.features[0].manual.map(feature => {
                 return (
-                  <div className="category">
+                  <div className="category" key={feature.id}>
                     <p>{feature.feature}</p>
                     <p className="element">{feature.content}</p>
                   </div>
@@ -119,7 +130,7 @@ const DetailItem = () => {
           </div>
         </div>
       </div>
-      <Slider detailItems={detailItems} />
+      <Slider recommend={detailItems.product.recommend} />
     </div>
   );
 };
