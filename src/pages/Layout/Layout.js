b234f1367c4114c9f75342cd510d536b0d1eb8b3
shs <hs.seo@kisos.net>
2022-06-30 14:23:21 +0900

    FIX : 네비게이션에서 장바구니 실행 할 수 있도록 수정

--- src/pages/Layout/Layout.js
+++ src/pages/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import ShoppingBasket from '../ShoppingBasket/ShoppingBasket';
-
+import Navigation from '../../components/Navigation/Navigation';
 const Layout = ({ children }) => {
   const [shoppingBasketShow, setShoppingBasketShow] = useState(false);
   return (
     <>
+      <Navigation setShoppingBasketShow={setShoppingBasketShow} />
+
       {shoppingBasketShow && (
         <ShoppingBasket
           show={shoppingBasketShow}
